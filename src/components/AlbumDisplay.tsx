76e87cc53efc7e71fe7167dde67c7300ff2dbc6b
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-08-26 04:09:47 +0800

    add skeleton for artist page

---
+++ src/components/AlbumDisplay.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+
+import { Shuffle } from "lucide-react";
+import Link from "next/link";
+
+import { type ArtistData } from "@/server/scrapers/azlyricsParser";
+import { api } from "@/utils/api";
+
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import { Skeleton } from "./ui/skeleton";
+
+type AlbumDisplayProps = {
+  album: NonNullable<ArtistData>["albums"][number];
+  artistKey: string;
+};
+
+export default function AlbumDisplay({ album, artistKey }: AlbumDisplayProps) {
+  const createRandomGame = api.game.createRandom.useMutation();
+
+  return (
+    <>
+      <Separator />
+      <div className="grid grid-cols-5">
+        <div className="col-span-2 flex flex-col items-center gap-6 max-md:ml-1.5 max-md:items-start max-md:gap-3">
+          {album.coverPhotoURL && (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              src={album.coverPhotoURL}
+              alt={album.name}
+              className="h-48 w-48 rounded-lg max-md:h-32 max-md:w-32 max-md:rounded-md"
+            />
+          )}
+          <h2 className="text-2xl font-bold max-md:text-xl">{album.name}</h2>
+          <Button
+            size="icon"
+            className="hidden max-md:inline-flex"
+            variant="secondary"
+            disabled={createRandomGame.isLoading}
+            onClick={() =>
+              createRandomGame.mutate({
+                artistKey,
+                album: album.name,
+              })
+            }
+          >
+            <Shuffle size="1.25em" />
+          </Button>
+        </div>
+
+        <div className="col-span-3 flex gap-3">
+          <Button
+            size="icon"
+            className="hidden md:inline-flex"
+            variant="secondary"
+            disabled={createRandomGame.isLoading}
+            onClick={() =>
+              createRandomGame.mutate({
+                artistKey,
+                album: album.name,
+              })
+            }
+          >
+            <Shuffle size="1.25em" />
+          </Button>
+          <ul>
+            {album.songs.map((song) => (
+              <AlbumSong song={song} key={song.title} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function AlbumSong({
+  song: { title, path },
+}: {
+  song: NonNullable<ArtistData["otherSongs"]>[number];
+}) {
+  return (
+    <Link href={`/play/${path}`} key={title}>
+      <li className="mb-1 rounded-lg px-3 py-1.5 transition-all hover:translate-x-2 hover:bg-accent hover:font-bold max-md:py-1 max-md:text-sm max-md:underline">
+        {title}
+      </li>
+    </Link>
+  );
+}
+
+export function AlbumDisplaySkeleton() {
+  return (
+    <>
+      <Separator />
+      <div className="grid grid-cols-5">
+        <div className="col-span-2 flex flex-col items-center gap-6 max-md:ml-1.5 max-md:items-start max-md:gap-3">
+          <Skeleton className="h-48 w-48 rounded-lg max-md:h-32 max-md:w-32 max-md:rounded-md" />
+          <Skeleton className="h-8 w-60 max-md:h-6 max-md:w-32" />
+          <Button
+            size="icon"
+            className="hidden max-md:inline-flex"
+            variant="secondary"
+            disabled
+          ></Button>
+        </div>
+
+        <div className="col-span-3 flex gap-3">
+          <Button
+            size="icon"
+            className="hidden md:inline-flex"
+            variant="secondary"
+            disabled
+          ></Button>
+          <ul>
+            {Array.from(Array(6), (_, index) => (
+              <AlbumSongSkeleton key={index} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function AlbumSongSkeleton() {
+  return (
+    <li className="w-full rounded-lg px-3 py-1.5 max-md:py-1 max-md:text-sm">
+      <Skeleton className="h-8 w-96" />
+    </li>
+  );
+}
--- src/pages/artists/[key].tsx
+++ src/pages/artists/[key].tsx
@@ -2,14 +2,17 @@ import React, { useEffect } from "react";
 
 import { Shuffle } from "lucide-react";
 import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
+import AlbumDisplay, {
+  AlbumDisplaySkeleton,
+  AlbumSong,
+} from "@/components/AlbumDisplay";
 import Header from "@/components/Header";
 import SEO from "@/components/SEO";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { type ArtistData } from "@/server/scrapers/azlyricsParser";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/utils/api";
 
 export default function ArtistPage() {
@@ -36,8 +39,20 @@ export default function ArtistPage() {
       </Head>
       <main className="flex min-h-screen flex-col items-center">
         <Header />
-        <div className="mx-auto mb-16 mt-8 max-w-4xl text-xl max-md:mx-1">
-          {artistData.data ? (
+        <div className="mx-auto mb-16 mt-8 w-full max-w-4xl text-xl max-md:mx-1">
+          {artistData.isLoading ? (
+            <>
+              <div className="mb-16 flex justify-between max-md:mb-8 max-md:px-4">
+                <Skeleton className="h-12 w-60 max-md:h-8 max-md:w-16" />
+                <Button size="sm" disabled className="w-28" />
+              </div>
+              <div className="flex flex-col gap-8 max-md:gap-4 max-md:px-2">
+                {Array.from(Array(3), (_, index) => (
+                  <AlbumDisplaySkeleton key={index} />
+                ))}
+              </div>
+            </>
+          ) : artistData.data ? (
             <>
               <div className="mb-16 flex justify-between max-md:mb-8 max-md:px-4">
                 <h1 className="text-4xl font-bold max-md:text-3xl">
@@ -55,59 +70,11 @@ export default function ArtistPage() {
               </div>
               <div className="flex flex-col gap-8 max-md:gap-4 max-md:px-2">
                 {artistData.data.albums.map((album) => (
-                  <React.Fragment key={album.name}>
-                    <Separator />
-                    <div key={album.name} className="grid grid-cols-5">
-                      <div className="col-span-2 flex flex-col items-center gap-6 max-md:ml-1.5 max-md:items-start max-md:gap-3">
-                        {album.coverPhotoURL && (
-                          // eslint-disable-next-line @next/next/no-img-element
-                          <img
-                            src={album.coverPhotoURL}
-                            alt={album.name}
-                            className="h-48 w-48 rounded-lg max-md:h-32 max-md:w-32 max-md:rounded-md"
-                          />
-                        )}
-                        <h2 className="text-2xl font-bold max-md:text-xl">
-                          {album.name}
-                        </h2>
-                        <Button
-                          size="icon"
-                          className="hidden max-md:inline-flex"
-                          variant="secondary"
-                          disabled={createRandomGame.isLoading}
-                          onClick={() =>
-                            createRandomGame.mutate({
-                              artistKey,
-                              album: album.name,
-                            })
-                          }
-                        >
-                          <Shuffle size="1.25em" />
-                        </Button>
-                      </div>
-                      <div className="col-span-3 flex gap-3">
-                        <Button
-                          size="icon"
-                          className="hidden md:inline-flex"
-                          variant="secondary"
-                          disabled={createRandomGame.isLoading}
-                          onClick={() =>
-                            createRandomGame.mutate({
-                              artistKey,
-                              album: album.name,
-                            })
-                          }
-                        >
-                          <Shuffle size="1.25em" />
-                        </Button>
-                        <ul>
-                          {album.songs.map((song) => (
-                            <AlbumSong song={song} key={song.title} />
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </React.Fragment>
+                  <AlbumDisplay
+                    key={album.name}
+                    album={album}
+                    artistKey={artistKey}
+                  />
                 ))}
                 {artistData.data?.otherSongs &&
                   artistData.data?.otherSongs.length > 0 && (
@@ -129,23 +96,14 @@ export default function ArtistPage() {
                   )}
               </div>
             </>
+          ) : artistData.isError ? (
+            <div className="mt-[30vh] text-center">
+              <h1 className="text-4xl font-bold">Error</h1>
+              <p className="text-xl">{artistData.error?.message}</p>
+            </div>
           ) : null}
         </div>
       </main>
     </>
-  );
-}
-
-function AlbumSong({
-  song: { title, path },
-}: {
-  song: NonNullable<ArtistData["otherSongs"]>[number];
-}) {
-  return (
-    <Link href={`/play/${path}`} key={title}>
-      <li className="mb-1 rounded-lg px-3 py-1.5 transition-all hover:translate-x-2 hover:bg-accent hover:font-bold max-md:py-1 max-md:text-sm max-md:underline">
-        {title}
-      </li>
-    </Link>
   );
 }
