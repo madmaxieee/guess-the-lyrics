b1555687ec8688f44956a8a9192f771a77cb79ef
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-04-14 21:15:05 +0800

    try to add dynamic og image to meta tags on game page

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,24 +1,46 @@
 import { env } from "@/env.mjs";
 
-export default function SEO() {
+type SEOProps = {
+  title?: string | null;
+  artist?: string | null;
+  coverPhotoUrl?: string | null;
+};
+
+export default function SEO({ title, artist, coverPhotoUrl }: SEOProps) {
+  const pageTitle = title
+    ? `guess the lyrics. | ${title}`
+    : "guess the lyrics.";
+
+  const ogUrl = (() => {
+    if (!artist && !title) return env.NEXT_PUBLIC_DOMAIN + "/og-image.png";
+    const ogUrl = new URL(env.NEXT_PUBLIC_DOMAIN + "/og/song");
+    if (artist) {
+      ogUrl.searchParams.append("artist", artist);
+    }
+    if (title) {
+      ogUrl.searchParams.append("song", title);
+    }
+    if (coverPhotoUrl) {
+      ogUrl.searchParams.append("image_url", coverPhotoUrl);
+    }
+    return ogUrl.toString();
+  })();
+
   return (
     <>
-      <meta name="description" content="guess the lyrics." />
+      <meta name="description" content={pageTitle} />
       <link rel="icon" href="/favicon.ico" />
       <meta
         name="keywords"
         content="guess the lyrics, guess, lyrics, game, music, songs, lyrics game, swiftie"
       />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="guess the lyrics." />
+      <meta property="og:title" content={pageTitle} />
       <meta
         property="og:description"
         content="Are you a real fan? Try to guess the lyrics of your favorite songs now! Challenge your friends and see who's the real one!"
       />
-      <meta
-        property="og:image"
-        content={env.NEXT_PUBLIC_DOMAIN + "/og-image.png"}
-      />
+      <meta property="og:image" content={ogUrl} />
       <meta property="og:image:width" content="1600" />
       <meta property="og:image:height" content="800" />
       <meta
