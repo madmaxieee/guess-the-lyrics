4194c342b5c63ee852b792fa129d559e630b9191
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-04-14 21:14:35 +0800

    comment out google ad stuf

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
-import { env } from "@/env.mjs";
+// import Script from "next/script";
+// import { env } from "@/env.mjs";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Script
-          id="adsense-id"
-          async
-          strategy="afterInteractive"
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_ADSENSE_ID}`}
-          crossOrigin="anonymous"
-        />
+        {/* <Script */}
+        {/*   id="adsense-id" */}
+        {/*   async */}
+        {/*   strategy="afterInteractive" */}
+        {/*   src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_ADSENSE_ID}`} */}
+        {/*   crossOrigin="anonymous" */}
+        {/* /> */}
         <Main />
         <NextScript />
       </body>
